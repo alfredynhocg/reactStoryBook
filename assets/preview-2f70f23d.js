5e7ebf06feff5a0680329144a1f7060fea1039bc
Alfredo Callizaya <alfredynho.cg@gmail.com>
2023-11-14 18:50:25 -0400

    Updates

---
+++ assets/preview-2f70f23d.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-51ba2b5a.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-228bd87b.js"),["./DocsRenderer-3PUGWF3O-228bd87b.js","./iframe-51ba2b5a.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./react-18-ba2f8a3a.js","./index-6a5bd4ef.js","./index-fb3fcfd3.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-2f70f23d.js.map
